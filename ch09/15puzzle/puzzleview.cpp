127f6460e51aa85ffd180d00059b177721de0c42
Graham Strickland <grahamhstrickland@gmail.com>
2022-08-20 14:01:04 +0200

    Updated 15puzzle with puzzleview function definitions

--- ch09/15puzzle/puzzleview.cpp
+++ ch09/15puzzle/puzzleview.cpp
@@ -1,10 +1,15 @@
 #include "puzzleview.h"
+#include <QMessageBox>
+#include <cstdlib>
+#include <ctime>
 
 int puzzleView::s_NumTiles = 15;
+int puzzleView::s_ShuffleMax = 50;
 
 PuzzleView::PuzzleView(QWidget *parent, PuzzleModel* model) :
     QWidget(parent), m_Model(model)
 {
+    //Set parent of model and connect signal to slot.
     m_Model->setParent(&this);
 
     QObject::connect(m_Model,
@@ -12,19 +17,31 @@ PuzzleView::PuzzleView(QWidget *parent, PuzzleModel* model) :
                      this,
                      SLOT(refresh()));
 
+    //Create QButtonGroup and add buttons; connect signals and slots.
     m_Buttons = new QButtonGroup();
     for (int tileNum = 1; tileNum <= s_NumTiles; tileNum++)
     {
         Tile* tile = new Tile(tileNum);
-        QObject::connect(tile,
-                         SIGNAL(QPushButton::clicked()),
-                         this,
-                         SLOT(tryToSlide(sender)));
         m_Buttons.addButton(tile);
     }
+    QObject::connect(m_Buttons,
+                     &QButtonGroup::buttonClicked,
+                     this,
+                     SLOT(tryToSlide(sender)));
+    QPushButton* shuffleButton = new QPushButton("Shuffle");
+    QObject::connect(shuffleButton,
+                     SIGNAL(QPushButton::clicked()),
+                     this,
+                     SLOT(shuffle()));
+    m_Buttons.addButton(shuffleButton);
+    QPushButton* quitButton = new QPushButton("Quit");
+    QObject::connect(quitButton,
+                     SIGNAL(QPushButton::clicked()),
+                     this,
+                     SLOT(close()));
+    m_Buttons.addButton(quitButton);
 
-    m_Layout = new QGridLayout();
-
+    //Create column and row layouts and add to QGridLayout.
     QVBoxLayout* col = new QVBoxLayout();
 
     QHBoxLayout* row = new QHBoxLayout();
@@ -54,5 +71,45 @@ PuzzleView::PuzzleView(QWidget *parent, PuzzleModel* model) :
     row->addWidget(m_Buttons.button(15));
     col->addLayout(row);
 
+    m_Layout = new QGridLayout();
+    setLayout(m_Layout);
+
     m_Layout->addLayout(col);
+
+    col = new QVBoxLayout();
+    col->addWidget(m_Buttons.button(16));
+    col->addWidget(m_Buttons.button(17));
+
+    m_Layout->addLayout(col);
+}
+
+void PuzzleView::refresh()
+{
+    QVBoxLayout tiles = m_Layout->itemAtPosition(0, 0);
+    QHBoxLayout row = tiles->itemAtPosition(0, 0);
+}
+
+void PuzzleView::shuffle()
+{
+    int tileNum = 0;
+    srand(time(0));
+
+    //Shuffle tiles s_ShuffleMax times.
+    for (int i = 0; i < s_ShuffleMax; i++)
+        do
+            tileNum = rand % s_NumTiles;
+        while (!m_Model->slide(m_Buttons.id(tileNum)));
+}
+
+void PuzzleView::tryToSlide(QAbstractButton *button)
+{
+    if (!m_Model->slide(m_Buttons.id(button)))
+    {
+        QMessageBox errorMsg = QMessageBox::information(
+                    &this,
+                    tr("Error"),
+                    tr("Tile " + button->text() + " has no empty neighbours.\n"),
+                    QMessageBox::Cancel);
+        errorMsg.exec();
+    }
 }
--- ch09/15puzzle/puzzleview.h
+++ ch09/15puzzle/puzzleview.h
@@ -15,23 +15,24 @@ public:
         //Constructor with parameters.
         //Postcondition: m_Model = model.
 
-    bool tryToSlide(QAbstractButton* button);
-        //Function to attempt a slide to an empty cell neighbouring
-        //button
-        //Postcondition: If neighbouring cell is empty, returned
-        //	true and called m_Model->slide() with button as argument;
-        //	otherwise returned false.
-
 public slots:
     void refresh();
         //Slot to adapt the display when gridChanged() called from
         //m_Model.
+
+    void shuffle();
+        //Slot to shuffle tiles when shuffleButton clicked.
+
+    void tryToSlide(QAbstractButton* button);
+        //Slot to attempt a slide to an empty cell neighbouring
+        //button
 
 private:
     PuzzleModel* m_Model;
     QGridLayout* m_Layout;
     QButtonGroup m_Buttons;
     static int s_NumTiles;
+    static int s_ShuffleMax;
 };
 
 #endif // PUZZLEVIEW_H
